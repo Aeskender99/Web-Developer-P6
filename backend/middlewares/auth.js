4c67d111525a301c6e81f0abc6d445270e82a6cf
Aeskender99 <alexandre.leonard.pro@gmail.com>
2022-12-13 09:53:52 +0100

    debogage

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,23 @@
 const jwt = require('jsonwebtoken');
 
+const dotenv = require("dotenv");
+dotenv.config();
+const TOKEN_KEY = process.env.TOKEN_KEY;
+
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; //Vient récupérer le token dans headers authorizations, coupe l'infos à l'espace, créer un tableau avec le split puis on récup l'entrée du token qui est en 2eme pos [1]
-    const decodedToken = jwt.verify(token, process.env.JTK);
+    const decodedToken = jwt.verify(token, TOKEN_KEY);
     const userId = decodedToken.userId;
+    req.auth = { userId };
     if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+      throw "ID invalide";
     } else {
       next();
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')});
+      error: new Error("Requête invalide !"),
+    });
   }
 };
-
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const tauxLimite = require('express-rate-limit'); // package pour prévenir des attaques par force brute
 const userControllers = require('../controllers/user');
 
-const connectLimatation = tauxLimite({
-    windowMs: 2 * 60 * 1000, // Temps défini pour tester l'application
-    max: 3 // Nbr de test max par adresse ip
-  });
 
-  router.post('/signup', userControllers.signup);
-  router.post('/login', userControllers.login);
+router.post('/signup', userControllers.signup);
+router.post('/login', userControllers.login);
   
 module.exports = router;        
